e5d84e1eafb4945fdc1298a71d36d90795b15f26
Oganes <og.marckaryan@yandex.ru>
2024-04-17 13:01:04 +0300

    add slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './style/style.css'
+const arrowLeft = document.querySelector('.slider-arrow__left');
+const arrowRight = document.querySelector('.slider-arrow__right');
+
+let currentIndex = 0;
+const carouselItems = document.querySelectorAll('.slider li');
+console.log(carouselItems)
+console.log(carouselItems.length)
+function goToSlide(index) {
+  if (index < 0) {
+    console.log(index)
+    index = carouselItems.length - 1;
+    console.log(index)
+
+  } else if (index >= carouselItems.length) {
+    index = 0;
+  }
+  currentIndex = index;
+  document.querySelector('.slider').style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+ 
+function goToNextSlide() {
+  goToSlide(currentIndex + 1);
+}
+arrowRight.addEventListener('click', goToNextSlide);
+
+function goToPrevSlide() {
+  goToSlide(currentIndex - 1);
+}
+arrowLeft.addEventListener('click', goToPrevSlide);
+
+
+// setInterval(goToNextSlide, 5000);
+
+
+
