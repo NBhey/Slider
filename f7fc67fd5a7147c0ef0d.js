f2ec902ae86274519cd8ad214fa1398ba9c2bf82
NBhey <NBhey@users.noreply.github.com>
2024-07-10 11:05:16 +0000

    Deploying to gh-pages from @ NBhey/Otus-lesson-21@309c312e47572fe03ecb33bdb193f330be325f3d 🚀

---
+++ f7fc67fd5a7147c0ef0d.js
@@ -0,0 +1,1 @@
+import"./style/style.css";import makeSlider from"./js/slider.js";import Cat from"./img/cat.jpg";import Cat2 from"./img/cat-second.jpg";import Cat3 from"./img/cat-third.jpg";makeSlider(Cat,Cat2,Cat3);
