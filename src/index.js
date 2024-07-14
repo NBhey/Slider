951770b95dad87e830c0730caf96f60f9c190a3f
Oganes <og.marckaryan@yandex.ru>
2024-07-14 19:37:22 +0300

    some cor-ns

--- src/index.js
+++ src/index.js
@@ -5,3 +5,6 @@ import Cat2 from './img/cat-second.jpg'
 import Cat3 from './img/cat-third.jpg'
 
 makeSlider(Cat,Cat2,Cat3)
+let element = document.querySelector('.feedback-footer')
+
+console.log(element)
