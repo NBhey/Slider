7c445d0b1f6f7dca98a7d676b33a9b27f49940ab
NBhey <NBhey@users.noreply.github.com>
2024-07-14 16:37:47 +0000

    Deploying to gh-pages from @ NBhey/Slider@db70fa053a547fdea300fa281a2c85b87adaded3 🚀

--- f7fc67fd5a7147c0ef0d.js
+++
@@ -1,1 +0,0 @@
-import"./style/style.css";import makeSlider from"./js/slider.js";import Cat from"./img/cat.jpg";import Cat2 from"./img/cat-second.jpg";import Cat3 from"./img/cat-third.jpg";makeSlider(Cat,Cat2,Cat3);
